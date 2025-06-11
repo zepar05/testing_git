1679c38515d933bf305798972193191f42f0cc45
zepar05 <azfarukail005@gmail.com>
2025-06-11 15:16:04 +0800

    add form2

--- testing_git/Form2.Designer.vb
+++ testing_git/Form2.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Form2
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,42 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.lblPaymentResult = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'lblPaymentResult
-        '
-        Me.lblPaymentResult.AutoSize = True
-        Me.lblPaymentResult.Location = New System.Drawing.Point(345, 102)
-        Me.lblPaymentResult.Name = "lblPaymentResult"
-        Me.lblPaymentResult.Size = New System.Drawing.Size(57, 20)
-        Me.lblPaymentResult.TabIndex = 0
-        Me.lblPaymentResult.Text = "Label1"
-        '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(253, 254)
+        Me.Button1.Location = New System.Drawing.Point(296, 149)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(204, 109)
-        Me.Button1.TabIndex = 1
+        Me.Button1.Size = New System.Drawing.Size(150, 106)
+        Me.Button1.TabIndex = 0
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Form1
+        'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.lblPaymentResult)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Name = "Form2"
+        Me.Text = "Form2"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents lblPaymentResult As Label
     Friend WithEvents Button1 As Button
 End Class
